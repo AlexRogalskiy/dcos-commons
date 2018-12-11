fc9ef55fde9750fb56f74552a0d1acb2790def7b
Jitendra Pawar <jitendra@infracloud.io>
2018-12-11 00:26:42 -0800

    Missed in last checkin. portworx_vol_function can be called from outside sdk_install.py on need basis.

--- testing/sdk_install.py
+++ testing/sdk_install.py
@@ -515,7 +515,7 @@ def portworx_cleanup():
         finally:
             sdk_utils.list_reserved_resources()
     # Call portworx specific cleanup routine at the end.
-    _portworx_volcleanup()
+    portworx_vol_cleanup()
 
 def merge_dictionaries(dict1, dict2):
     if (not isinstance(dict2, dict)):
