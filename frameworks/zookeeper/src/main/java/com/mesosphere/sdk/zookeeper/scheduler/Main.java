8983f001bc29fa533b01667102ac190da76c865a
Dinesh Israni <disrani@portworx.com>
2019-07-30 16:49:48 -0700

    [Zookeeper] Fix build and static analysis checks
    Also removed unused SERVICE_USER

--- frameworks/zookeeper/src/main/java/com/mesosphere/sdk/zookeeper/scheduler/Main.java
+++ frameworks/zookeeper/src/main/java/com/mesosphere/sdk/zookeeper/scheduler/Main.java
@@ -1,6 +1,5 @@
 package com.mesosphere.sdk.zookeeper.scheduler;
 
-import com.google.common.base.Joiner;
 import com.mesosphere.sdk.scheduler.DefaultScheduler;
 import com.mesosphere.sdk.scheduler.SchedulerBuilder;
 import com.mesosphere.sdk.scheduler.SchedulerConfig;
@@ -8,35 +7,42 @@ import com.mesosphere.sdk.scheduler.SchedulerRunner;
 import com.mesosphere.sdk.specification.DefaultServiceSpec;
 import com.mesosphere.sdk.specification.yaml.RawServiceSpec;
 
+import com.google.common.base.Joiner;
+
 import java.io.File;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Zookeeper service.
  */
-public class Main {
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            throw new IllegalArgumentException("Expected one file argument, got: " + Arrays.toString(args));
-        }
-        SchedulerRunner
-                .fromSchedulerBuilder(createSchedulerBuilder(new File(args[0])))
-                .run();
-    }
+public final class Main {
+  private Main() {}
 
-    private static SchedulerBuilder createSchedulerBuilder(File yamlSpecFile)
-            throws Exception {
-        SchedulerConfig schedulerConfig = SchedulerConfig.fromEnv();
-        RawServiceSpec rawServiceSpec = RawServiceSpec.newBuilder(yamlSpecFile).build();
-        List<String> zkNodes = ZookeeperConfigUtils.getZookeeperNodes(rawServiceSpec.getName());
-        SchedulerBuilder schedulerBuilder = DefaultScheduler.newBuilder(
-                DefaultServiceSpec
-                        .newGenerator(rawServiceSpec, schedulerConfig, yamlSpecFile.getParentFile())
-                        .setAllPodsEnv("ZOOKEEPER_SERVERS", Joiner.on('\n').join(zkNodes))
-                        .build(),
-                schedulerConfig)
-                .setPlansFrom(rawServiceSpec);
-
-        return schedulerBuilder;
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1) {
+      throw new
+          IllegalArgumentException("Expected one file argument, got: " + Arrays.toString(args));
     }
+    SchedulerRunner
+        .fromSchedulerBuilder(createSchedulerBuilder(new File(args[0])))
+        .run();
+  }
+
+  private static SchedulerBuilder createSchedulerBuilder(File yamlSpecFile) throws Exception {
+    SchedulerConfig schedulerConfig = SchedulerConfig.fromEnv();
+    RawServiceSpec rawServiceSpec = RawServiceSpec.newBuilder(yamlSpecFile).build();
+    List<String> zkNodes = ZookeeperConfigUtils.getZookeeperNodes(
+        rawServiceSpec.getName(),
+        schedulerConfig);
+    SchedulerBuilder schedulerBuilder = DefaultScheduler.newBuilder(
+        DefaultServiceSpec
+            .newGenerator(rawServiceSpec, schedulerConfig, yamlSpecFile.getParentFile())
+            .setAllPodsEnv("ZOOKEEPER_SERVERS", Joiner.on('\n').join(zkNodes))
+            .build(),
+        schedulerConfig)
+        .setPlansFrom(rawServiceSpec);
+
+    return schedulerBuilder;
+  }
 }
--- frameworks/zookeeper/src/main/java/com/mesosphere/sdk/zookeeper/scheduler/ZookeeperConfigUtils.java
+++ frameworks/zookeeper/src/main/java/com/mesosphere/sdk/zookeeper/scheduler/ZookeeperConfigUtils.java
@@ -1,47 +1,56 @@
 package com.mesosphere.sdk.zookeeper.scheduler;
 
 import com.mesosphere.sdk.http.EndpointUtils;
+import com.mesosphere.sdk.scheduler.SchedulerConfig;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * This class returns a collection of seed nodes for zookeeper cluster
+ * This class returns a collection of seed nodes for zookeeper cluster.
  * It returns in a format that the zookeeper config understands
  */
-public class ZookeeperConfigUtils {
-    /**
-     * Returns the list of zookeeper servers configuration
-     * e.g. 'server.1=zookeeper-0-node.autoip...:2888:3888'
-     */
-    public static List<String> getZookeeperNodes(String serviceName) {
-        List<String> zkNodes = new ArrayList<>();
-        for (int i = 0; i < getNodeCount(); i++) {
-            String taskDns = EndpointUtils.toAutoIpHostname(serviceName, String.format("zookeeper-%d-node", i));
-            zkNodes.add(String.format("server.%d=%s:%s:%s",
-                i + 1, taskDns, getLeaderPort(), getLeaderElectionPort()));
-        }
-        return zkNodes;
-    }
+public final class ZookeeperConfigUtils {
+  private ZookeeperConfigUtils() {}
 
-    /**
-     * Returns the number of seed nodes in the cluster
-     */
-    private static int getNodeCount() {
-        return Integer.parseInt(System.getenv("NODE_COUNT"));
+  /**
+   * Returns the list of zookeeper servers configuration
+   * e.g. 'server.1=zookeeper-0-node.autoip...:2888:3888'
+   */
+  public static List<String> getZookeeperNodes(
+          String serviceName,
+          SchedulerConfig schedulerConfig)
+  {
+    List<String> zkNodes = new ArrayList<>();
+    for (int i = 0; i < getNodeCount(); i++) {
+      String taskDns = EndpointUtils.toAutoIpHostname(
+          serviceName,
+          String.format("zookeeper-%d-node", i),
+          schedulerConfig);
+      zkNodes.add(String.format("server.%d=%s:%s:%s",
+          i + 1, taskDns, getLeaderPort(), getLeaderElectionPort()));
     }
+    return zkNodes;
+  }
 
-    /**
-     * Returns the port followers use to connect to the leader
-     */
-    private static String getLeaderPort() {
-        return System.getenv("TASKCFG_ALL_LEADER_PORT");
-    }
+  /**
+   * Returns the number of seed nodes in the cluster.
+   */
+  private static int getNodeCount() {
+    return Integer.parseInt(System.getenv("NODE_COUNT"));
+  }
 
-    /**
-     * Returns the port used for leader election
-     */
-    private static String getLeaderElectionPort() {
-        return System.getenv("TASKCFG_ALL_LEADER_ELECTION_PORT");
-    }
+  /**
+   * Returns the port followers use to connect to the leader.
+   */
+  private static String getLeaderPort() {
+    return System.getenv("TASKCFG_ALL_LEADER_PORT");
+  }
+
+  /**
+   * Returns the port used for leader election.
+   */
+  private static String getLeaderElectionPort() {
+    return System.getenv("TASKCFG_ALL_LEADER_ELECTION_PORT");
+  }
 }
